4be3a1e6fb1c26a5eeb48d66e1b1e31cec090d68
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-08-18 17:57:05 +0530

    commit

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -11,14 +11,14 @@ class Product {
 }
 
 List<Product> productList = [
-  Product('item1', "100\$", 'red', 0,'asset/images/s1.jpg'),
-  Product('item2', "200\$", 'blue', 0,'asset/images/s2.jpg'),
-  Product('item3', "300\$", 'green', 0,'asset/images/s3.jpg'),
-  Product('item4', "400\$", 'yellow', 0,'asset/images/s4.jpg'),
-  Product('item5', "500\$", 'purple', 0,'asset/images/s5.jpg'),
-  Product('item6', "600\$", 'black', 0,'asset/images/s6.jpg'),
-  Product('item7', "700\$", 'brown', 0,'asset/images/s7.jpg'),
-  Product('item8', "800\$", 'pink', 0,'asset/images/s8.jpg'),
-  Product('item9', "900\$", 'cyan', 0,'asset/images/s9.jpg'),
+  Product('item1', "100\$", 'red', 0,'asset/images/s1.png'),
+  Product('item2', "200\$", 'blue', 0,'asset/images/s2.png'),
+  Product('item3', "300\$", 'green', 0,'asset/images/s3.png'),
+  Product('item4', "400\$", 'yellow', 0,'asset/images/s4.png'),
+  Product('item5', "500\$", 'purple', 0,'asset/images/s5.png'),
+  Product('item6', "600\$", 'black', 0,'asset/images/s6.png'),
+  Product('item7', "700\$", 'brown', 0,'asset/images/s7.png'),
+  Product('item8', "800\$", 'pink', 0,'asset/images/s8.png'),
+  Product('item9', "900\$", 'cyan', 0,'asset/images/s9.png'),
 ];
 
